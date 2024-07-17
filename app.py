5cdc437130385f34c2f95c9f2852a0d74be4c2e7
Darshan Mangroliya <darshanmangroliya07@gmail.com>
2024-07-17 17:20:40 -0400

    added selenium

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 app = Flask(__name__)
+
+options=webdriver.EdgeOptions()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-extensions")
+options.add_argument("--no-sandbox")
+
+driver=webdriver.Edge(options=options)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb'
+
+    driver.get("https://www.google.com")
+    return driver.title
 
 
 if __name__ == "__main__":
